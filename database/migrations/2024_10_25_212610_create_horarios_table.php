b04da210e8fd0c95e6486553d95baf1d015897c0
EleFLFM <luifermejia1104@gmail.com>
2024-10-25 16:59:41 -0500

    primer commit empieza la configuracion en las rutas

---
+++ database/migrations/2024_10_25_212610_create_horarios_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('horarios', function (Blueprint $table) {
+            $table->id();
+            $table->date('fecha');
+            $table->time('hora');
+            $table->enum('estado', ['Disponible', 'No Disponible'])->default('Disponible');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('horarios');
+    }
+};
