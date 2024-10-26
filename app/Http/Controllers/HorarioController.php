b3e7b64fc20a82c5dba24a56ceeb2968d9362d64
EleFLFM <luifermejia1104@gmail.com>
2024-10-25 19:59:17 -0500

    Añadir cambios en layouts, sidebar, horarios e incluir estilos

--- app/Http/Controllers/HorarioController.php
+++ app/Http/Controllers/HorarioController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Horario;
 use Illuminate\Http\Request;
 
 class HorarioController extends Controller
 {
-    //
+    public function index()
+    {
+        $horarios = Horario::all(); // Asegúrate de que se obtienen los horarios
+        return view('horarios.index', compact('horarios'));
+    }
 }
