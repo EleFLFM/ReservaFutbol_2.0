5f66ae81bfff3f224510b565f15f92d389799236
fray david herrera duarte <148037427+Frayherrera@users.noreply.github.com>
2024-10-25 20:22:13 -0500

    se agrego auth y los roles y permisos

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -20,5 +20,13 @@ class RoleSeeder extends Seeder
         // Crear roles
         $adminRole = Role::create(['name' => 'administrador']);
         $clientRole = Role::create(['name' => 'cliente']);
+
+        // Crear permisos
+        Permission::create(['name' => 'crear horarios'])->syncRoles($adminRole);
+        Permission::create(['name' => 'aprobar reservas'])->syncRoles($adminRole);
+        Permission::create(['name' => 'rechazar reservas'])->syncRoles($adminRole);
+        Permission::create(['name' => 'ver horarios'])->syncRoles($adminRole, $clientRole);
+        Permission::create(['name' => 'hacer reserva'])->syncRoles($adminRole, $clientRole);
+        Permission::create(['name' => 'cancelar reserva'])->syncRoles($adminRole, $clientRole);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,25 +2,21 @@
 
 use App\Http\Controllers\HorarioController;
 use App\Http\Controllers\ReservaController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 Route::get('/horarios', [HorarioController::class, 'index']);
 Route::post('/reservas', [ReservaController::class, 'store']);
 Route::get('/admin/reservas', [ReservaController::class, 'index']);
 Route::post('/admin/reservas/{id}/aprobar', [ReservaController::class, 'aprobar']);
 Route::post('/admin/reservas/{id}/rechazar', [ReservaController::class, 'rechazar']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
