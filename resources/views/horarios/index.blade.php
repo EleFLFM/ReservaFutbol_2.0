3e237abad4dfe51bf6ab137b9851c6c840c83267
fray david herrera duarte <148037427+Frayherrera@users.noreply.github.com>
2024-11-06 14:45:12 -0500

    gnn

--- resources/views/horarios/index.blade.php
+++ resources/views/horarios/index.blade.php
@@ -15,7 +15,7 @@
 <!DOCTYPE html>
 <html>
 
-<head>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Horarios Disponibles</title>
@@ -181,7 +181,7 @@
             top: 3px;
         }
     </style>
-</head>
+
 
 <body>
     <div class="horarios-container">
